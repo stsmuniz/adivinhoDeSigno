604c07ab805600b5c091e44201c0c2342fea2321
Salustiano Muniz <stsmuniz@gmail.com>
2016-08-11 01:46:16 -0300

    images and stuff for first functional version

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,43 @@
 
 function sorteiaSigno() {
-	var signo = [	'Áries', 'Touro', 'Gêmeos', 'Câncer',	'Leão', 'Virgem', 'Libra', 'Escorpião',	'Sagitário', 'Capricórnio', 'Aquário', 'Peixes'];
+	var signo = [
+	{name: 'Aries', classname: 'aries'},
+	{name: 'Touro', classname: 'taurus'},
+	{name: 'Gêmeos', classname: 'gemini'},
+	{name: 'Câncer', classname: 'cancer'},
+	{name: 'Leão', classname: 'leo'},
+	{name: 'Virgem', classname: 'virgo'},
+	{name: 'Libra', classname: 'libra'},
+	{name: 'Escorpião', classname: 'scorpion'},
+	{name: 'Sagitário', classname: 'sagittarius'},
+	{name: 'Capricórnio', classname: 'capricorn'},
+	{name: 'Aquário', classname: 'aquarius'},
+	{name: 'Peixes', classname: 'pisces'}
+	];
 
-  return signo[Math.round(Math.random() * 11 )];
+	return signo[Math.round(Math.random() * 11 )];
 }
 
+$(document).ready(function() {
+	$('#textoSigno #signos').fadeOut(0);
+})
 
-document.getElementById('sol').innerText = sorteiaSigno();
-document.getElementById('ascendente').innerText = sorteiaSigno();
-document.getElementById('lua').innerText = sorteiaSigno();
+
+$('#revelaSignoBtn').click(function() {
+	$('#textoSigno #signos').fadeOut('slow', function() {
+		$('#sol').attr('class', 'simboloSigno');
+		var sol = sorteiaSigno();
+		$('#sol').text(sol.name);
+		$('#sol').addClass(sol.classname);
+		$('#ascendente').attr('class', 'simboloSigno');
+		var ascendente = sorteiaSigno();
+		$('#ascendente').text(ascendente.name);
+		$('#ascendente').addClass(ascendente.classname);
+		$('#lua').attr('class', 'simboloSigno');
+		var lua = sorteiaSigno();
+		$('#lua').text(lua.name);
+		$('#lua').addClass(lua.classname);
+	});
+
+	$('#textoSigno #signos').fadeIn(1500);
+})
